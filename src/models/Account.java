a12eecb4693905c10c7765f798d69ef7351b188c
Vortex <example@gmail.com>
2025-11-08 00:35:15 +0530

    This is the commit for completion of the account class

--- src/models/Account.java
+++ src/models/Account.java
@@ -19,7 +19,7 @@ public Account(String accountNumber , String accountHolderName , double initialB
             this.accountHolderName=accountHolderName;
             this.balance=initialBalance;
             this.transactionHistory= new ArrayList<>();
-            addTransaction(" Account created with intial balance: $"+ initialBalance);
+            addTransaction(" Account created with initial balance: $"+ initialBalance);
 }
 // Encapsulation : Public getter
     public String getAccountNumber() {
@@ -28,10 +28,95 @@ public Account(String accountNumber , String accountHolderName , double initialB
     public String getAccountHolderName() {
                 return accountHolderName;
     }
+    public double getBalance(){
+                return balance;
+    }
+
+    public List<String> getTransactionHistory() {
+        return new ArrayList<>(transactionHistory);         // the copy of the encapsulation
+    }
+
+    // protected setter for derived classes
+    protected void setBalance(double balance){
+                this.balance=balance;
+    }
+    protected void addTransaction(String transaction){
+        String timestamp = java.time.LocalDateTime.now().format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        transactionHistory.add("[" + timestamp + "] " + transactionHistory);
+    }
+    public abstract void withdraw(double amount); // for child classes
+
+    public  void deposit(double amount){
+        if (amount >0){
+            addTransaction("Deposited : $" + amount);
+            System.out.println("Successfully Deposited : $"+ amount);
+        }
+        else{
+            System.out.println("Invalid Deposit amount!!!");
+        }
+
+    }
+    public void displayAccountInfo(){
+        System.out.println("<== Account Info ==>");
+        System.out.println("Account Number:"+ accountNumber);
+        System.out.println("Account HolderName:"+accountHolderName);
+        System.out.println("Balance: $" + balance);
+        System.out.println("AccountType:"+ this.getClass().getSimpleName());
+    }
+
+    public void displayTransactionHistory(){
+        System.out.println("<==TransactionHistory==>");
+        if (transactionHistory.isEmpty()){
+            System.out.println("No TransactionHistory");
+        }
+        else{
+            for (String Transaction : transactionHistory){
+                System.out.println(Transaction);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 }
+
+
+
 
 
 
